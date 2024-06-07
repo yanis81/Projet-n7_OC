362bbc1d3f284027694e647102099d80f766ba9d
Yanis Lartigue <lartigueyanis@yahoo.com>
2024-06-07 22:49:02 +0200

    API Post mise en place

--- backend/database/mongo.js
+++ backend/database/mongo.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 
+
 const DB_URL = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@${process.env.DB_DOMAIN}`;
 
 mongoose
@@ -9,12 +10,30 @@ mongoose
   .then(() => console.log("Connexion à MongoDB réussie !"))
 .catch(() => console.log("Connexion à MongoDB échouée !"));
 
-
-const UserSchema = new mongoose.Schema({
-  email: String,
-  password: String
-});
-
+//Shema des données pour les Users
+ const UserSchema = new mongoose.Schema({
+   email: String,
+   password: String
+  });
+//Création du model pour la base de donnée 
 const User = mongoose.model("User",UserSchema);
 
-module.exports = { User };
+//Shema des données pour les livres
+const BookShema = new mongoose.Schema({
+  userId: String,
+  title: String,
+  author: String,
+  year: Number,
+  genre: String,
+  ratings: [
+    {
+      userId: String,
+      grade: Number
+    }
+  ],
+  averageRating:Number  
+});
+//Création du model pour la base de donnée 
+ const Book = mongoose.model("Book",BookShema)
+
+module.exports = { User, Book };
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const app = express();
-const { User } = require("./database/mongo"); // permet de ce connecter avec la Database mongo
+const { User, Book } = require("./database/mongo"); // permet de ce connecter avec la Database mongo
 const cors = require("cors");
 const bcrypt = require("bcrypt");
 const { books } = require("./database/books");
+const multer = require("multer");
 
 const PORT = 4000; // indication du port utiliser
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    const fileName = file.originalname.toLowerCase() + Date.now() + ".jpg";
+    cb(null, Date.now() + "-" + fileName);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+});
+
 app.use(cors());
 app.use(express.json()); //Permet de parser le JSON du body
+//app.use("/images", express.static("uploads"));
 
 function sayHi(req, res) {
   res.send("Hello Word");
@@ -76,23 +92,37 @@ async function login(req, res) {
   });
 }
 
+//compare le mot de passe initial et le mot de passe hasher
 function isPasswordCorrect(password, hash) {
   return bcrypt.compareSync(password, hash);
 }
 
+//permet de récupérer tous les livres en DB
 function getBooks(req, res) {
   res.send(books);
 }
 
-function postBooks(req, res) {
-  
+//permet de poster de nouveaux livres
+async function postBooks(req, res) {
+  //const file = req.file;
+  const stringifiedBook = req.body.book;
+  const book = JSON.parse(stringifiedBook); //parse les données du livre en JSON
+  //book.imageURL = file.path;
+  try {
+    const result = await Book.create(book); //crée le livre dans la base de donnée
+    res.send({ message: "Livre enregistrer", book: result }); //renvoi un message de confirmation et les detail du livre poster
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("quelque chose a mal tourner" + e.message); //permet d'eviter les crash du server et d'avoir un message des que ça ne vas pas
+  }
 }
 
+// API
 app.get("/", sayHi);
 app.post("/api/auth/signup", signUp); // Requete POST pour l'incription
 app.post("/api/auth/login", login); // Requete POST pour la connexion
-app.get("/api/books", getBooks);
-app.post("/api/books", postBooks);
+app.get("/api/books", getBooks); // Requete GET pour les livres
+app.post("/api/books", upload.single("image"), postBooks); //Requete POST pour les livres
 
 app.listen(PORT, function () {
   console.log(`Server is running on: ${PORT}`);
